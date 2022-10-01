89eac2558851c4a79d67f76f073377e674ae801f
pabloariata <pabloariata@gmail.com>
2022-10-01 18:26:26 -0300

    Insert data without calling another service

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from '../pokemon/pokemon.module';
 
 @Module({
   controllers: [SeedController],
-  providers: [SeedService]
+  providers: [SeedService],
+  imports: [
+    PokemonModule
+  ]
 })
 export class SeedModule {}
